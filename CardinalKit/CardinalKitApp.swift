72e6ca162f88ddefac4be8b0dc95ed6466507379
TheJohnLe <jle17@mail.sfsu.edu>
2020-11-06 11:43:35 -0800

    Initial Commit

---
+++ CardinalKit/CardinalKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardinalKitApp.swift
+//  CardinalKit
+//
+//  Created by John Le on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct CardinalKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
