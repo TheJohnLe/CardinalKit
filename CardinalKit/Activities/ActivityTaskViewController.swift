02364563b5cb13bbefabb4ed21a4ff7d2d5640f7
TheJohnLe <jle17@mail.sfsu.edu>
2020-11-06 18:09:55 -0800

    Updated task-card documentation

--- CardinalKit/Activities/ActivityTaskViewController.swift
+++ CardinalKit/Activities/ActivityTaskViewController.swift
@@ -21,25 +21,33 @@ final class ActivityTaskViewController: OCKDailyPageViewController {
             guard let tasks = try? result.get() else { return }
             
             tasks.forEach { task in
+                /*
+                  1. Create new card of the View Controller containing the specified task
+                  2. Append card to the listViewController
+                 */
                 switch task.id {
-                // 1. Create new card of the View Controller containing the specified task
-                // 2. Append card to the listViewController
+                // setup breathing card
                 case CareStoreReferenceManager.TaskIdentifiers.breathingEpisodes.rawValue:
                     let breathingCard = BreathingSurveyViewController(viewSynchronizer: BreathingSurveyViewSynchronizer(), task: task, eventQuery: OCKEventQuery(for: Date()), storeManager: self.storeManager)
                     listViewController.appendViewController(breathingCard, animated: false)
+                // setup coughing card
                 case CareStoreReferenceManager.TaskIdentifiers.coughingEpisodes.rawValue:
                     let coughingCard = OCKButtonLogTaskViewController(task: task,
                                 eventQuery: .init(for: date),storeManager: self.storeManager)
                     listViewController.appendViewController(coughingCard, animated: false)
+                // setup stretching card
                 case CareStoreReferenceManager.TaskIdentifiers.stretchingEpisodes.rawValue:
                     let stretchingCard = OCKSimpleTaskViewController(task: task, eventQuery: .init(for: date), storeManager: self.storeManager);
                     listViewController.appendViewController(stretchingCard, animated: false)
+                // setup walking card
                 case CareStoreReferenceManager.TaskIdentifiers.walkingEpisodes.rawValue:
                     let walkingCard = WalkingSurveyViewController(viewSynchronizer: WalkingSurveyViewSynchronizer(), task: task, eventQuery: OCKEventQuery(for: Date()), storeManager: self.storeManager)
                     listViewController.appendViewController(walkingCard, animated: false)
+                // setup test card
                 case CareStoreReferenceManager.TaskIdentifiers.testEpisodes.rawValue:
                     let testCard = OCKInstructionsTaskViewController(task: task, eventQuery: .init(for: date), storeManager: self.storeManager)
                     listViewController.appendViewController(testCard, animated: false)
+                // setup back pain card
                 case CareStoreReferenceManager.TaskIdentifiers.backPainEpisodes.rawValue:
                     let backPainCard = PainSurveyViewController(viewSynchronizer: PainSurveyViewSynchronizer(), task: task, eventQuery: OCKEventQuery(for: Date()), storeManager: self.storeManager)
                     listViewController.appendViewController(backPainCard, animated: true)
